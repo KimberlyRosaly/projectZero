6772abd78986afde19610971288542274c1f50cb
KimberlyRosaly <01010110.v@gmail.com>
2023-03-22 23:51:46 -0400

    flesh out Next.JS documentation example with my intended landing page

--- hih/pages/index.js
+++ hih/pages/index.js
@@ -1,4 +1,26 @@
+/* ==========================================
+ROOT PAGE
+==========================================*/
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+  <div>
+    HEART ♥ IS HOME
+  </div>
+  )
 }
 export default HomePage
+/* ==========================================
+STEP (1)
+  I N T R O   C A R D > .
+    FULLSCREEN HERO TEXT APPEARS ON SCREEN, 
+      vanilla css text transform ?
+      or 
+      functional component > 
+        timer w/ fade, then 'display: none' attribute?
+    TEXT FADES TO BLACK
+STEP (2)
+    PUSH USER TO '/signup'
+    PUSH USER TO '/login'
+    PUSH USER TO '/home' > 
+      our beloved logged-in user's personal dashboard
+========================================== */
