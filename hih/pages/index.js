1ab553c0c73d30a9f2e37b00740280c59a07436b
KimberlyRosaly <01010110.v@gmail.com>
2023-03-22 21:33:21 -0400

    follow Next.JS documentation > throw in their example React functions to test out routing and 'pages' concept

--- hih/pages/index.js
+++ hih/pages/index.js
@@ -1,13 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from '../src/App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+function HomePage() {
+  return <div>Welcome to Next.js!</div>
+}
+export default HomePage
