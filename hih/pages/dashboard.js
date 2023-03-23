086a95a14fffffef38f9b55c2054ed303f5192e6
KimberlyRosaly <01010110.v@gmail.com>
2023-03-22 23:52:52 -0400

    swap out documentation example with a lovely new dashboard function, as is intention

---
+++ hih/pages/dashboard.js
@@ -0,0 +1,16 @@
+/* ==========================================
+DASHBOARD
+==========================================*/
+export default function Dashboard() {
+    return (
+        <div>
+          DASHBOARD ✏️
+        </div>
+        )
+}
+/* ==========================================
+CONTAINERS
+    NAVIGATION
+    TODO LIST | EARNED STICKERS
+========================================== */
+  
