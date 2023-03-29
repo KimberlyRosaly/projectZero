c947756d83e976627bb425c5f46037805509ba04
KimberlyRosaly <01010110.v@gmail.com>
2023-03-28 20:33:43 -0400

    create a button component to be imported around the app

---
+++ hih/src/components/MyButton.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+/* ==========================================
+ROOT PAGE
+==========================================*/
+function MyButton() {
+  return (
+    <div>
+      <Link href="dashboard">B U T T O N to user clickies</Link>
+    </div>
+  )
+}
+export default MyButton
+
