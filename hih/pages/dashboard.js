057c38d328bfa4ee495dc5f4bb5e7ce3f78e5a6d
KimberlyRosaly <01010110.v@gmail.com>
2023-03-31 01:07:47 -0400

    button component now handles prop values, added some wandering thoughts

--- hih/pages/dashboard.js
+++ hih/pages/dashboard.js
@@ -1,18 +1,27 @@
 import MyButton from "../src/components/MyButton"
 /* ==========================================
-DASHBOARD
+D A S H B O A R D  PAGE - '/dashboard'
 ==========================================*/
 export default function Dashboard() {
     return (
         <div>
           <div>DASHBOARD ✏️</div>
-          <MyButton />          
+          <MyButton targetURL={'/'} displayText={"Let's go home"} />          
         </div>
         )
 }
 /* ==========================================
 CONTAINERS
     NAVIGATION
-    TODO LIST | EARNED STICKERS
+    TODO LIST
+    (un)+EARNED STICKERS
+
+thought..
+  maybe i can draw something like..
+  a house in some grass...make it move a bit..
+  fireflies that flicker,strobe, super slow with some keyframes? gotta really look into that..
+  zoom in a moment for a transition.. 
+
+another random allusion :  do you remember that red riding hood game? yeah, it was kinda pretty.. immersive..made me want to stay awhile..
 ========================================== */
   
